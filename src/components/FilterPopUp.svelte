eb4dc751745de2a92619999804ab17bf9b589003
sreen020 <sjoerdreen@gmail.com>
2021-05-18 16:58:20 +0200

    filter pop up component + styling (not done)

---
+++ src/components/FilterPopUp.svelte
@@ -0,0 +1,81 @@
+<section class="filter-popup">
+	<h1>Groepen</h1>
+	<form>
+		<fieldset>
+			<legend>Groepen</legend>
+
+			<input type="checkbox" id="group-1-2" name="filter-group" value="12" />
+			<label for="group-1-2">Groep 1 en 2</label>
+			<input type="checkbox" id="group-3-4" name="filter-group" value="34" />
+			<label for="group-3-4">Groep 3 en 4</label>
+			<input type="checkbox" id="group-5-6" name="filter-group" value="45" />
+			<label for="group-5-6">Groep 5 en 6</label>
+			<input type="checkbox" id="group-7-8" name="filter-group" value="67" />
+			<label for="group-7-8">Groep 7 en 8</label>
+			<input type="checkbox" id="all-groups" name="filter-group" value="all" />
+			<label for="all-groups">Alle jaarlagen</label>
+		</fieldset>
+
+		<button class="submit-btn" type="submit">Toepassen</button>
+		<button class="cancel-btn">Annuleren</button>
+	</form>
+</section>
+<div class="black-overlay" />
+
+<style>
+	.filter-popup {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 80%;
+		background-color: white;
+		z-index: 1;
+		padding: 1rem;
+	}
+
+	.filter-popup h1 {
+		text-align: center;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+	}
+
+	fieldset {
+		display: flex;
+		flex-direction: column;
+		border: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	label {
+		padding: 1rem 2rem;
+		border: 1px solid black;
+		border-radius: 1rem;
+		margin-bottom: 1rem;
+		cursor: pointer;
+		transition: 0.2s;
+	}
+
+	input {
+		display: none;
+	}
+
+	input:checked + label {
+		border: 1px solid var(--color-light-orange);
+		color: var(--color-light-orange);
+	}
+
+	.black-overlay {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.6);
+		z-index: 0;
+	}
+</style>
