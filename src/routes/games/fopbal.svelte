65951e37577d841073fd6822284af2d09ac760be
Jochem Vogel <voge0031@hz.nl>
2021-05-19 10:48:11 +0200

    Remove object typing

--- src/routes/games/fopbal.svelte
+++ src/routes/games/fopbal.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import GamePage from '../../shared/GamePage.svelte';
 
-	let game: Object = {
+	let game = {
 		title: 'Fopbal',
 		description:
 			'Maak een grote kring met één iemand in het midden (de fopper), die heeft de bal. De leerlingen in de kring (de vangers) houden de handen achter de rug. De leerling in het midden mag steeds de bal echt of nep (fop) gooien. Gooit de leerling nep, maar komen jouw handen wel zichtbaar achter jouw rug vandaag? Dan ben je af en ga je zitten. Gooit de leerling de bal echt maar vang jij deze niet? Dan ben je ook af en ga je zitten. Vang je de bal wel of blijven je handen netjes achter je rug als de fopper jou fopt? Dan blijf je meedoen. De laatste die over blijft wint en wordt de nieuwe fopper nemen.',
