306a6bc190e4c9134d9ca1f274eac8b150bd0a50
Jochem Vogel <voge0031@hz.nl>
2021-05-21 09:22:40 +0200

    Update lint stage - Test 3

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    linters: {
+      '**/*.+(js|md|ts|css|yml|yaml|html|json|svelte)': [
+        'eslint --fix',
+        'prettier --write',
+        'git add',
+      ],
+    },
+  };
