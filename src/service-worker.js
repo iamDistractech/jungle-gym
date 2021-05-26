8e11358a817980451fb5553156e1746910375a66
sreen020 <sjoerdreen@gmail.com>
2021-05-26 17:15:54 +0200

    modified service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,4 @@
-import { build, files, timestamp } from '$service-worker';
-
-const assets = [...build, '/fallback.html', '/'];
+import { build, timestamp } from '$service-worker';
 
 const staticCacheName = `staticCache-v${timestamp}`;
 const dynamicCacheName = `dynamicCache-v${timestamp}`;
@@ -10,7 +8,7 @@ self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches
 			.open(staticCacheName)
-			.then((cache) => cache.addAll(assets))
+			.then((cache) => cache.addAll(build))
 			.then(self.skipWaiting())
 	);
 });
@@ -29,26 +27,17 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-	console.log(build);
-
 	evt.respondWith(
-		caches
-			.match(evt.request)
-			.then((cacheRes) => {
-				return (
-					cacheRes ||
-					fetch(evt.request).then((fetchRes) => {
-						return caches.open(dynamicCacheName).then((cache) => {
-							cache.put(evt.request.url, fetchRes.clone());
-							return fetchRes;
-						});
-					})
-				);
-			})
-			.catch((error) => {
-				return caches
-					.open(staticCacheName)
-					.then((cache) => cache.match('/_app/pages/offline.svelte-dc2af9e5.js'));
-			})
+		caches.match(evt.request).then((cacheRes) => {
+			return (
+				cacheRes ||
+				fetch(evt.request).then((fetchRes) => {
+					return caches.open(dynamicCacheName).then((cache) => {
+						cache.put(evt.request.url, fetchRes.clone());
+						return fetchRes;
+					});
+				})
+			);
+		})
 	);
 });
