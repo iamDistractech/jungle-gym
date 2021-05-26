916c561d9e4fafdef2f6c0cfb91988d810adf1c8
sreen020 <sjoerdreen@gmail.com>
2021-05-26 13:31:43 +0200

    cache all responses

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,12 @@
-import { build, timestamp } from '$service-worker';
+import { build, files, timestamp } from '$service-worker';
 
-const assets = [...build, '/offline'];
+const assets = ['/offline', '/'];
 
-const staticCacheName = `staticCache-${timestamp}`;
+const staticCacheName = `staticCache-v${timestamp}`;
+const dynamicCacheName = `dynamicCache-v${timestamp}`;
 
 self.addEventListener('install', (event) => {
+	console.log('install');
 	event.waitUntil(
 		caches
 			.open(staticCacheName)
@@ -14,10 +16,29 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
+	console.log('Activate');
 	event.waitUntil(
 		caches.keys().then((keys) => {
 			return Promise.all(
 				keys.filter((key) => key !== staticCacheName).map((key) => caches.delete(key))
+			);
+		})
+	);
+});
+
+self.addEventListener('fetch', (evt) => {
+	console.log('event', evt.request);
+
+	evt.respondWith(
+		caches.match(evt.request).then((cacheRes) => {
+			return (
+				cacheRes ||
+				fetch(evt.request).then((fetchRes) => {
+					return caches.open(dynamicCacheName).then((cache) => {
+						cache.put(evt.request.url, fetchRes.clone());
+						return fetchRes;
+					});
+				})
 			);
 		})
 	);
