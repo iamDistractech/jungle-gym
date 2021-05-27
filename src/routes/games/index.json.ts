2695aadcc6a9fe663a5be9c849c195916cbcb700
Thijs Spijker <thijs@tsnetwork.nl>
2021-05-27 11:44:06 +0200

    fix: removed unused import

--- src/routes/games/index.json.ts
+++ src/routes/games/index.json.ts
@@ -1,4 +1,4 @@
-import type { EndpointOutput, RequestHandler } from '@sveltejs/kit';
+import type { RequestHandler } from '@sveltejs/kit';
 import { api } from './_api';
 
 export const get: RequestHandler = async (request) => {
