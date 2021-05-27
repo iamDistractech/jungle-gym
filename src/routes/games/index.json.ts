eeece8a669ce384a8d58348a215f110ea2d891c4
Thijs Spijker <thijs@tsnetwork.nl>
2021-05-27 11:29:48 +0200

    fix: format

--- src/routes/games/index.json.ts
+++ src/routes/games/index.json.ts
@@ -1,8 +1,7 @@
-import type { EndpointOutput, RequestHandler } from "@sveltejs/kit";
-import { api } from "./_api";
+import type { EndpointOutput, RequestHandler } from '@sveltejs/kit';
+import { api } from './_api';
 
-
-export const get: RequestHandler = async(request) => {
-	const response = await api(request, 'games')
-	return response
-}
+export const get: RequestHandler = async (request) => {
+	const response = await api(request, 'games');
+	return response;
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,35 @@
 <script context="module" lang="ts">
 	import type { Load } from '@sveltejs/kit';
 
-	export const load: Load = async ({page, fetch}) => {
-		const {query} = page
-		const res = await fetch(`/api/games.json?${query.toString()}`)
+	export const load: Load = async ({ page, fetch }) => {
+		const { query } = page;
+		const res = await fetch(`/api/games.json?${query.toString()}`);
 
-		if(res.ok) {
-			const games = await res.json()
+		if (res.ok) {
+			const games = await res.json();
 
 			return {
 				props: {
 					games,
 					query
 				}
-			}
+			};
 		}
 
-		const { message } = await res.json()
+		const { message } = await res.json();
 
 		return {
 			error: new Error(message),
 			props: {
 				query
 			}
-		}
-
-	}
-
+		};
+	};
 </script>
 
 <script>
 	import GameList from '$lib/GameList/GameList.svelte';
 	import GameListFilter from '$lib/GameList/GameListFilter.svelte';
-
 
 	export let games;
 	export let query;
