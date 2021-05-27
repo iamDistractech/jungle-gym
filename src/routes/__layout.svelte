27143ccb9764fa9682b87238d9b468e4ae171777
Jochem Vogel <voge0031@hz.nl>
2021-05-27 09:08:12 +0200

    Update header styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,19 +6,3 @@
 <main>
 	<slot />
 </main>
-
-<style>
-	header {
-		font-family: var(--font-heading);
-		display: flex;
-		flex-direction: column-reverse;
-		padding: 2rem 0 1rem;
-	}
-
-	header h2 {
-		margin: 0;
-		color: var(--color-grey);
-		font-weight: 400;
-		font-size: 1em;
-	}
-</style>
