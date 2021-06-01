73cb5ff199bfeac7d4a28acc882da5cad9436a21
Thijs Spijker <thijs@tsnetwork.nl>
2021-06-01 14:54:29 +0200

    fix: turned on strapi cms

--- src/lib/Env.js
+++ src/lib/Env.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = import.meta.env.VITE_API_URL;
-// export const BASE_API_URL = import.meta.env.VITE_STRAPI_API_URL;
+// export const BASE_API_URL = import.meta.env.VITE_API_URL;
+export const BASE_API_URL = import.meta.env.VITE_STRAPI_API_URL;
