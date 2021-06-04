751272da530ca6191af7a1c3343e2361193e1a97
Thijs Spijker <thijs@tsnetwork.nl>
2021-06-04 18:33:27 +0200

    fix: return of patch

--- src/lib/Utils/strapiPatch.ts
+++ src/lib/Utils/strapiPatch.ts
@@ -4,27 +4,25 @@ export function strapiPatchAll(strapiGames: []): Game[] {
 	return strapiGames.map(strapiPatchSingle);
 }
 
-export function strapiPatchSingle(params:type): Game {
-	(strapiGame: Record<string, string | [] | Record<string, string>>): Game => {
-		const game: Game = {
-			id: typeof strapiGame.id === 'string' ? strapiGame.id : '',
-			slug: typeof strapiGame.slug === 'string' ? strapiGame.slug : '',
-			name: typeof strapiGame.name === 'string' ? strapiGame.name : '',
-			description: typeof strapiGame.description === 'string' ? strapiGame.description : '',
-			category: typeof strapiGame.category.name === 'string' ? strapiGame.category.name : '',
-			materials: checkMaterial(strapiGame.materials),
-			minimumPlayers:
-				typeof strapiGame.minimumPlayers === 'string'
-					? Number(strapiGame.minimumPlayers)
-					: undefined,
-			targetGroup: Array.isArray(strapiGame.targetGroup)
-				? strapiGame.targetGroup.map((a: Record<string, string>) => parseInt(a.group))
-				: [],
-			rules: Array.isArray(strapiGame.rules) ? strapiGame.rules : [],
-			variation: Array.isArray(strapiGame.variation) ? strapiGame.variation : []
-		};
-		return game;
-	}
+export function strapiPatchSingle(strapiGame: Record<string, string | [] | Record<string, string>>): Game {
+	const game: Game = {
+		id: typeof strapiGame.id === 'string' ? strapiGame.id : '',
+		slug: typeof strapiGame.slug === 'string' ? strapiGame.slug : '',
+		name: typeof strapiGame.name === 'string' ? strapiGame.name : '',
+		description: typeof strapiGame.description === 'string' ? strapiGame.description : '',
+		category: typeof strapiGame.category.name === 'string' ? strapiGame.category.name : '',
+		materials: checkMaterial(strapiGame.materials),
+		minimumPlayers:
+			typeof strapiGame.minimumPlayers === 'string'
+				? Number(strapiGame.minimumPlayers)
+				: undefined,
+		targetGroup: Array.isArray(strapiGame.targetGroup)
+			? strapiGame.targetGroup.map((a: Record<string, string>) => parseInt(a.group))
+			: [],
+		rules: Array.isArray(strapiGame.rules) ? strapiGame.rules : [],
+		variation: Array.isArray(strapiGame.variation) ? strapiGame.variation : []
+	};
+	return game;
 }
 
 function checkMaterial(strapiMaterials: string | [] | Record<string, string>): Material[] {
