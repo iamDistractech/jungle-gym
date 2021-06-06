80fd7750841b6d297902cb85c5542fe9143c8fb7
Thijs Spijker <thijs@tsnetwork.nl>
2021-06-06 19:23:13 +0200

    fix: old API configuration

--- src/lib/Env.js
+++ src/lib/Env.js
@@ -1,5 +1,4 @@
-// export const BASE_API_URL = import.meta.env.VITE_API_URL;
-export const BASE_API_URL = import.meta.env.VITE_STRAPI_API_URL;
+export const BASE_API_URL = import.meta.env.VITE_API_URL;
 export const SESSION_DB_URL = import.meta.env.VITE_SESSION_DB_URL;
 export const SESSION_DB_PORT = import.meta.env.VITE_SESSION_DB_PORT;
 export const SESSION_DB_USERNAME = import.meta.env.VITE_SESSION_DB_USERNAME;
