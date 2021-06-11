7962b79458cb9f26d5ca83bf6d910ab60d1fc1f5
Jochem Vogel <voge0031@hz.nl>
2021-06-11 10:20:40 +0200

    Add submit function on submit
    Just a console log that will log all the values

--- src/lib/GameViews/GameReview.svelte
+++ src/lib/GameViews/GameReview.svelte
@@ -14,10 +14,60 @@
 		explanationForm.style.display = 'block';
 		explanationForm.scrollIntoView({ behavior: 'smooth' });
 	}
+
+	function submitSmallForm() {
+		const firstStepForm = document.querySelector('.first-step');
+		const gameName = window.location.pathname.split('/')[2];
+
+		const overallAnswer = {
+			type: 'short-form',
+			game: gameName,
+			emoji: firstStepForm['emoji-overall'].value
+		};
+
+		console.log(overallAnswer);
+	}
+
+	function submitLongForm() {
+		const explanationForm = document.querySelector('.explanation-container');
+
+		const descriptionAnswers = {
+			emoji: explanationForm['emoji-description'].value,
+			explanation: explanationForm['explanation-description'].value
+		};
+
+		const executionAnswers = {
+			emoji: explanationForm['emoji-execution'].value,
+			explanation: explanationForm['explanation-execution'].value
+		};
+
+		const kidsAnswers = {
+			emoji: explanationForm['emoji-kids'].value,
+			explanation: explanationForm['explanation-kids'].value
+		};
+
+		const gameName = window.location.pathname.split('/')[2];
+
+		const formResults = {
+			type: 'long-form',
+			game: gameName,
+			descriptionAnswers,
+			executionAnswers,
+			kidsAnswers
+		};
+
+		console.log(formResults);
+	}
 </script>
 
 <form name="emoji" class="first-step" action="">
-	<input type="radio" id="sad" name="emoji" value="sad" on:click|once={toggleConfirmation} />
+	<input
+		type="radio"
+		id="sad"
+		name="emoji-overall"
+		value="sad"
+		on:click|once={toggleConfirmation}
+	/>
 	<label for="sad">
 		<img src="/icons/GameReview/sad.svg" alt="" />
 	</label>
@@ -25,7 +75,7 @@
 	<input
 		type="radio"
 		id="neutral"
-		name="emoji"
+		name="emoji-overall"
 		value="neutral"
 		on:click|once={toggleConfirmation}
 	/>
@@ -33,7 +83,13 @@
 		<img src="/icons/GameReview/surprised.svg" alt="" />
 	</label>
 
-	<input type="radio" id="happy" name="emoji" value="happy" on:click|once={toggleConfirmation} />
+	<input
+		type="radio"
+		id="happy"
+		name="emoji-overall"
+		value="happy"
+		on:click|once={toggleConfirmation}
+	/>
 	<label for="happy">
 		<img src="/icons/GameReview/happy.svg" alt="" />
 	</label>
@@ -47,81 +103,78 @@
 		<p>Bedankt, wil jij jouw keuze toelichten?</p>
 		<div>
 			<button class="yes-btn" on:click|preventDefault={() => toggleExplanation()}>Ja</button>
-			<button class="no-btn" type="submit">Nee, bedankt</button>
+			<button class="no-btn" on:click|preventDefault={submitSmallForm} type="submit"
+				>Nee, bedankt</button
+			>
 		</div>
 	</div>
 </form>
 
-<form class="explanation-container" action="">
+<form class="explanation-container" action="" on:submit|preventDefault={submitLongForm}>
 	<fieldset>
 		<legend>Was de omschrijving van het spel duidelijk? [1/3]</legend>
-		<input type="radio" id="sad-description" name="emoji-description" value="sad-description" />
+		<input type="radio" id="sad-description" name="emoji-description" value="sad" />
 		<label for="sad-description">
 			<img src="/icons/GameReview/sad.svg" alt="" />
 		</label>
 
-		<input
-			type="radio"
-			id="neutral-description"
-			name="emoji-description"
-			value="neutral-description"
-		/>
+		<input type="radio" id="neutral-description" name="emoji-description" value="neutral" />
 		<label for="neutral-description">
 			<img src="/icons/GameReview/surprised.svg" alt="" />
 		</label>
 
-		<input type="radio" id="happy-description" name="emoji-description" value="happy-description" />
+		<input type="radio" id="happy-description" name="emoji-description" value="happy" />
 		<label for="happy-description">
 			<img src="/icons/GameReview/happy.svg" alt="" />
 		</label>
 
-		<label for="explanation" class="fieldset-title">Licht je antwoord toe</label>
+		<label for="explanation-description" class="fieldset-title">Licht je antwoord toe</label>
 		<small>(optioneel)</small>
-		<textarea name="explanation" cols="30" rows="10" />
+		<textarea name="explanation-description" cols="30" rows="10" />
 	</fieldset>
 
 	<fieldset>
 		<legend>Hoe was het spel in de praktijk? [2/3]</legend>
-		<input type="radio" id="sad-execution" name="emoji-execution" value="sad-execution" />
+		<input type="radio" id="sad-execution" name="emoji-execution" value="sad" />
 		<label for="sad-execution">
 			<img src="/icons/GameReview/sad.svg" alt="" />
 		</label>
 
-		<input type="radio" id="neutral-execution" name="emoji-execution" value="neutral-execution" />
+		<input type="radio" id="neutral-execution" name="emoji-execution" value="neutral" />
 		<label for="neutral-execution">
 			<img src="/icons/GameReview/surprised.svg" alt="" />
 		</label>
 
-		<input type="radio" id="happy-execution" name="emoji-execution" value="happy-execution" />
+		<input type="radio" id="happy-execution" name="emoji-execution" value="happy" />
 		<label for="happy-execution">
 			<img src="/icons/GameReview/happy.svg" alt="" />
 		</label>
 
-		<label for="explanation" class="fieldset-title">Licht je antwoord toe</label>
+		<label for="explanation-execution" class="fieldset-title">Licht je antwoord toe</label>
 		<small>(optioneel)</small>
-		<textarea name="explanation" cols="30" rows="10" />
+		<textarea name="explanation-execution" cols="30" rows="10" />
 	</fieldset>
 
 	<fieldset>
 		<legend>Vonden de kinderen het een leuk spel? [3/3]</legend>
-		<input type="radio" id="sad-kids" name="emoji-kids" value="sad-kids" />
+		<input type="radio" id="sad-kids" name="emoji-kids" value="sad" />
 		<label for="sad-kids">
 			<img src="/icons/GameReview/sad.svg" alt="" />
 		</label>
 
-		<input type="radio" id="neutral-kids" name="emoji-kids" value="neutral-kids" />
+		<input type="radio" id="neutral-kids" name="emoji-kids" value="neutral" />
 		<label for="neutral-kids">
 			<img src="/icons/GameReview/surprised.svg" alt="" />
 		</label>
 
 		<input type="radio" id="happy-kids" name="emoji-kids" value="happy-kids" />
-		<label for="happy-kids">
+		<label for="happy">
 			<img src="/icons/GameReview/happy.svg" alt="" />
 		</label>
 
-		<label for="explanation" class="fieldset-title">Licht je antwoord toe</label>
+		<label for="explanation-kids" class="fieldset-title">Licht je antwoord toe</label>
 		<small>(optioneel)</small>
-		<textarea name="explanation" cols="30" rows="10" />
+		<textarea name="explanation-kids" cols="30" rows="10" />
 	</fieldset>
 
 	<SubmitButton>Versturen</SubmitButton>
