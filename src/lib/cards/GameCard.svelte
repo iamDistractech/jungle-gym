59c3d418a3e50cf8adc925bc42220799b9ac08af
Jochem Vogel <voge0031@hz.nl>
2021-06-14 10:55:04 +0200

    Add green dropshadow to card

--- src/lib/cards/GameCard.svelte
+++ src/lib/cards/GameCard.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import type { Game } from '$lib/games';
-	import CardLabel from '$lib/shared/label/CardLabel.svelte';
-	import OfflineLabel from '$lib/shared/label/OfflineLabel.svelte';
-	import { formatTargetGroups } from '$lib/utils/format';
+	import CardLabel from '$lib/shared/Label/CardLabel.svelte';
+	import OfflineLabel from '$lib/shared/Label/OfflineLabel.svelte';
+	import { formatTargetGroups } from '$lib/Utils/format';
 
 	export let game: Game;
 
@@ -27,10 +27,8 @@
 		border-radius: 1.5em;
 		padding: 1em;
 		justify-content: space-between;
-		box-shadow: 0px 2px 4px 2px rgba(21, 45, 21, 0.1);
-		/* filter: drop-shadow(0 0.2em 0.2em hsl(92, 30%, 64%)) alternatief shadow */
+		filter: drop-shadow(0 0.2em 0.2em hsl(92, 30%, 64%));
 	}
-
 	ul {
 		display: flex;
 		flex-wrap: wrap;
